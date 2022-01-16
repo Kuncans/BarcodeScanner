fd6ffec4c55d7f5a7857c6db85390ff6a69ba2fc
Kuncans <akafauna@gmail.com>
2022-01-16 16:03:03 +0000

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Duncan Kent on 16/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
